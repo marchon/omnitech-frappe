60a78facb77b4c701079eef153e2ee6061fc8132
mbauskar <mbauskar@gmail.com>
2015-10-21 18:30:44 +0530

    changes in user.js

--- frappe/core/doctype/user/user.js
+++ frappe/core/doctype/user/user.js
@@ -1,5 +1,3 @@
-{% include 'omnitechapp/js/custom_user.js' %};
-
 cur_frm.cscript.onload = function(doc, dt, dn) {
 	if(has_common(user_roles, ["Administrator", "System Manager"])) {
 		if(!cur_frm.roles_editor) {
@@ -326,3 +324,51 @@ frappe.RoleEditor = Class.extend({
 		this.perm_dialog.$wrapper.find('.modal-dialog').css("width", "800px");
 	}
 });
+
+frappe.ui.form.on("User",{
+    onload: function(frm){
+        // hide or unhide role according to package
+        // if(user != "Administrator")
+		get_modules();
+    },
+    reload: function(frm){
+        // hide or unhide role according to package
+		get_modules();
+    }
+});
+
+get_modules = function(){
+    // get allowed module for users from Package Detail
+    frappe.call({
+        method: "omnitechapp.omnitechapp.doctype.package_detail.package_detail.get_package_detail",
+        callback: function(r){
+            if(r.message){
+                pkg = r.message;
+                hide_roles(pkg.roles_to_hide);
+                hide_modules(pkg.allowed_modules);
+            }
+        }
+    });
+}
+
+hide_roles = function(roles){
+    if(roles){
+        $.each(roles, function(i, role){
+            role_div = $("[data-user-role='"+ role +"']");
+            role_div.css("display", "none");
+            role_div.find("input[type='checkbox']").prop("checked",0);
+        });
+    }
+}
+
+hide_modules = function(allowed_modules){
+    if(allowed_modules){
+        $.each(keys(frappe.boot.modules), function(i, mod){
+            if(!in_list(allowed_modules, mod)){
+                console.log(mod);
+                $("[data-module='"+ mod +"']").prop("checked",0);
+                $("[data-module='"+ mod +"']").parents().eq(2).css("display","none");
+            }
+        })
+    }
+}
